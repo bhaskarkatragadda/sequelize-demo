7964859c4a4212039312f99219411b09a82edf9c
Bhaskar Katragadda <katragaddabhaskar1729@gmail.com>
2024-07-04 23:12:15 +0530

    setup sequelize logger

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 // // Replace with your database credentials and configuration
-const sequelize = new Sequelize('sqlite::memory:')
+const sequelize = new Sequelize('sqlite::memory:', {
+    logging: console.log,
+})
 
 module.exports = sequelize;
